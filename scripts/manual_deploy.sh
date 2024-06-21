28760535b860124f7338c8c723d0904a101b274c
tonilustfilms <toni@erikalustfilms.com>
2024-06-21 13:08:09 +0200

    change script

--- scripts/manual_deploy.sh
+++ scripts/manual_deploy.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
-
 # Compilar el proyecto Astro
 echo "Compilando proyecto Astro..."
 yarn build
-
-# Copiar archivos de 'dist' a 'public' (sobrescribiendo existentes)
-echo "Copiando archivos de 'dist' a 'public'..."
-rm -rf public  # Eliminar 'public' existente si es necesario
-cp -r dist public
-
 # Desplegar a Firebase Hosting localmente
 echo "Desplegando a Firebase Hosting localmente..."
 firebase deploy --only hosting -m "Manual deploy from local"
